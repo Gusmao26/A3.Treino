52b7363cbb3fac384e89d128e44357876633b08c
Gusmao26 <guigusmao12@gmail.com>
2025-05-20 19:31:25 -0300

    feat: Usuario

---
+++ src/main/java/Models/Usuario.java
@@ -0,0 +1,29 @@
+package Models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+
+    public Usuario(int id, String nome, String senha, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+    }
+
+    public Usuario(){
+
+    }
+
+}
