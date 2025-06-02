2e9fee13dea239f19ac5fe0894b6d7a0b4e15c63
Gusmao26 <guigusmao12@gmail.com>
2025-06-02 20:16:08 -0300

    feat: estrutura de telas da aplicação e menu

--- src/main/java/GerenciadorProjetoApp.java
+++ src/main/java/GerenciadorProjetoApp.java
@@ -3,22 +3,54 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class GerenciadorProjetoApp {
-    private static EntityManagerFactory emf;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Locale;
+
+public class GerenciadorProjetoApp extends JFrame {
+
+    private static final String EMPT_SCREEN = "EMPT_SCREEN";
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
+    public GerenciadorProjetoApp(){
+        setTitle("Gerenciador de Projetos");
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel emptyPanel = new JPanel(new BorderLayout());
+        emptyPanel.add(new JLabel("Bem-vindo! Use o menu para navegar!", SwingConstants.CENTER),
+                BorderLayout.CENTER);
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        mainPanel.add(emptyPanel, EMPT_SCREEN);
+
+        JMenu menu = new JMenu("Menu");
+        JMenuItem listUsersItem = new JMenuItem("Listar Usuários");
+        JMenuItem exitItem = new JMenuItem("Sair");
+
+        menu.add(listUsersItem);
+        menu.add(exitItem);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(menu);
+
+        setJMenuBar(menuBar);
+
+        exitItem.addActionListener(event -> {
+            dispose();
+        });
+
+
+        add(mainPanel);
+    }
 
     public static void main(String[] args) {
-        emf = Persistence.createEntityManagerFactory("PU");
-        EntityManager em = emf.createEntityManager();
-
-        Usuario usuario1 = new Usuario(1, "Dick Vigarista",
-                "dickvigarista@gmail.com", "12345");
-        Usuario usuario2 = new Usuario(2, "Penélope Charmosa",
-                "penelopecharmosa@gmail.com", "54321");
-
-        // Persistir no BD
-        em.getTransaction().begin();
-        em.persist(usuario1);
-        em.persist(usuario2);
-        em.getTransaction().commit();
+        SwingUtilities.invokeLater(() -> {
+            new GerenciadorProjetoApp().setVisible(true);
+        });
     }
 }
